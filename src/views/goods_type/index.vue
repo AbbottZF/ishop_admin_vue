732c9f9369ba269a0f22d16fdbaa429b3e680a27
AbbottZF <39108584+AbbottZF@users.noreply.github.com>
2018-12-21 19:31:20 +0800

    12.21.1

--- src/views/goods_type/index.vue
+++ src/views/goods_type/index.vue
@@ -1,42 +1,34 @@
 <template>
   <div class="app-container">
-    <div class="filter-container">
+    <div class="filter-container" style="margin: 10px;">
       <el-input @keyup.enter.native="onSearch" style="width: 200px;" class="filter-item" placeholder="请填写名称" v-model="formData.name">
       </el-input>
       <el-select clearable style="width: 200px" class="filter-item" v-model="formData.parent_id" placeholder="选择一级分类">
         <el-option v-for="item in formData.parent" :key="item" :label="item" :value="item">
         </el-option>
       </el-select>
-      <!-- <el-select clearable class="filter-item" style="width: 130px" v-model="listQuery.type" :placeholder="$t('table.type')">
-        <el-option v-for="item in  calendarTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key">
-        </el-option>
-      </el-select>
-      <el-select @change='onSearch' style="width: 140px" class="filter-item" v-model="listQuery.sort">
-        <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key">
-        </el-option>
-      </el-select>
-      <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="onSearch">{{$t('table.search')}}</el-button>
-      <el-button class="filter-item" style="margin-left: 10px;" @click="handleCreate" type="primary" icon="el-icon-edit">{{$t('table.add')}}</el-button>
-      <el-button class="filter-item" type="primary" :loading="downloadLoading" v-waves icon="el-icon-download" @click="handleDownload">{{$t('table.export')}}</el-button>
-      <el-checkbox class="filter-item" style='margin-left:15px;' @change='tableKey=tableKey+1' v-model="showReviewer">{{$t('table.reviewer')}}</el-checkbox> -->
+      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="onSearch">搜索</el-button>
+      <el-button class="filter-item" style="margin-left: 10px;" @click="create" type="primary" icon="el-icon-plus">新增</el-button>
     </div>
-    <!-- <el-form :inline="true" :model="formInline" class="demo-form-inline">
-      <el-form-item label="名称">
-        <el-input type="text" v-model="formInline.name" placeholder="请填写名称" maxlength="10" size="medium" @keyup.enter.native="onSearch"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSearch">查询</el-button>
-      </el-form-item>
-    </el-form> -->
-    <el-table ref="multipleTable" :data="tableData3" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+    <el-table ref="multipleTable" :data="goods_list" tooltip-effect="dark" style="width: 100%;margin: 15px;" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55"></el-table-column>
-      <el-table-column label="日期" width="120">
+      <el-table-column prop="index" label="序号" width="120"/>
+      <el-table-column prop="name" label="名称" width="120"/>
+      <el-table-column prop="address" label="地址" show-overflow-tooltip/>
+      <el-table-column prop="sort" label="排序"/>
+      <el-table-column prop="status_text" label="状态"/>
+      <el-table-column label="录入时间" width="120">
         <template slot-scope="scope">{{ scope.row.date }}</template>
       </el-table-column>
-      <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-      <el-table-column prop="address" label="地址" show-overflow-tooltip></el-table-column>
+      <el-table-column label="操作" align="center" width="300" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button type="info" size="mini" @click="editInfo(scope.row)" icon="el-icon-tickets">详情</el-button>
+          <el-button type="primary" size="mini" @click="editInfo(scope.row)" icon="el-icon-edit">编辑</el-button>
+          <el-button type="danger" size="mini" @click="deleteInfo(scope.row)" icon="el-icon-delete">删除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
-    <div class="pagination-container">
+    <div class="pagination-container" style="margin: 5px 10px 10px 10px;text-align:left;padding-left:0px;">
       <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="page" :page-sizes="[10,20,30, 50]" :page-size="limit" layout="total, sizes, prev, pager, next, jumper" :total="total">
       </el-pagination>
     </div>
@@ -60,10 +52,14 @@
           name: '',
           region: ''
         },
-        tableData3: [{
+        goods_list: [{
+          index:1,
           date: '2016-05-03',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          address: '上海市普陀区金沙江路 1518 弄',
+          status_text:'正常',
+          sort:1,
+          action:'正常',
         }],
         multipleSelection: []
       }
